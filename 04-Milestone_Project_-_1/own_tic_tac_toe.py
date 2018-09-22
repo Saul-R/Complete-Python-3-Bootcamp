b92db3eea489244917e585fc527093d94a4d3a87
saulr <estifjarris@gmail.com>
2018-09-22 13:24:07 +0200

    MILESTONE-1 First sketch with everything done

--- 04-Milestone_Project_-_1/own_tic_tac_toe.py
+++ 04-Milestone_Project_-_1/own_tic_tac_toe.py
@@ -1,4 +1,8 @@
 def create_board():
+    """
+    Creates empty board
+    :return: Dictionary with new tictactoe board
+    """
     board={
         1: " ",
         2: " ",
@@ -12,21 +16,56 @@ def create_board():
     }
     return board
 
+
 def input_replay():
-    pass
+    return True
 
-def input_move():
-    pass
 
-def check_game_status():
-    pass
+def input_move(board,player):
+    """
+    Inputs a movement on the board for a player
+    :param board: The tictactoe board
+    :param player: The player's symbol
+    :return: The board with the new movenment
+    """
+    position = input('Input the position on the board: ')
+    board[position] = player
+    return board
+
+
+def check_game_status(board):
+    return (rows_match(board)) or (columns_match(board)) or (diagonals_match(board))
+
+
+def cells_match(cell1, cell2, cell3):
+    return (cell1 == cell2) and (cell2 == cell3)
+
+
+def rows_match(board):
+    row1 = cells_match(board[1], board[2], board[2])
+    row2 = cells_match(board[4], board[5], board[6])
+    row3 = cells_match(board[7], board[8], board[9])
+    return row1 or row2 or row3
+
+def columns_match(board):
+    col1 = cells_match(board[1], board[4], board[7])
+    col2 = cells_match(board[2], board[5], board[8])
+    col3 = cells_match(board[3], board[6], board[9])
+    return col1 or col2 or col3
+
+
+def diagonals_match(board):
+    diag1 = cells_match(board[1], board[5], board[9])
+    diag2 = cells_match(board[4], board[5], board[6])
+    return diag1 or diag2
+
 
 def print_board(board):
-    '''
+    """
     Receives a tictactoe board and prints it.
     :param board: The board dictionary
     :return: void
-    '''
+    """
     print("-------------")
     print("--  BOARD  --")
     print("-------------")
@@ -35,32 +74,30 @@ def print_board(board):
     print("| {} | {} | {} |".format(board[7],board[8],board[9]))
     print("-------------")
 
-def print_winner():
-    pass
 
-def print_finish():
-    pass
+def check_tie(board):
+    return not any(x == " " for x in board.values())
 
-def print_tie():
-    pass
 
-def play_a_game():
+def play_a_game(board):
+    players = ['X', 'O']
     while True:
-        input=input_move()
-        print_board()
-        game_status=check_game_status(board)
-        if game_status == 'Winner':
-            print_winner()
-            break
-        elif game_status == 'Tie'
-            print_tie()
+        for player in players:
+            board = input_move(board,player)
+            winner = check_winner(board)
+            if winner:
+                print('The player {} has won!')
+                break
+            tie = check_tie(board)
+            if tie:
+                print('Cannot do any movement. This is a tie')
+                break
 
-if __name__=="main"
 
-    create_board()
+if __name__=="main":
+    board = create_board()
     keep_playing = True
-
-    while keep_playing == True:
-        play_a_game()
-        keep_playing=input_replay()
-    print_finish()
+    while keep_playing:
+        play_a_game(board)
+        keep_playing = input('Keep playing? (True / False): ')
+    print('Thanks for playing.')
