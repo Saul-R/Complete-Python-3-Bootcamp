8f78fb09fc743234d2a6bf467db6dcd7b15a28f7
saulr <estifjarris@gmail.com>
2018-10-01 13:26:05 +0200

    MILESTONE-2 Tests for get cards method

--- 08-Milestone_Project_-_2/blackjack/tests/unit/test_player.py
+++ 08-Milestone_Project_-_2/blackjack/tests/unit/test_player.py
@@ -1,5 +1,6 @@
 import unittest
 from blackjack.roles.human_player import BlackjackPlayer
+from blackjack.elements.deck import PokerCard, PokerDeck
 
 
 class TestBlackjackPlayer(unittest.TestCase):
@@ -7,3 +8,11 @@ class TestBlackjackPlayer(unittest.TestCase):
     def test_create_player(self):
         my_test_player = BlackjackPlayer(name= 'John', starting_money=100)
         self.assertEqual('John', my_test_player.name)
+        self.assertEqual(len(my_test_player.hand), 0)
+
+    def test_get_card(self):
+        my_test_player = BlackjackPlayer(name= 'John', starting_money=100)
+        my_deck = PokerDeck()
+        my_test_player.get_card(my_deck)
+        self.assertEqual(len(my_test_player.hand), 1)
+        self.assertIsInstance(my_test_player.hand[0], PokerCard)
