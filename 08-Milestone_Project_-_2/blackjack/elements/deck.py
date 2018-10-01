aaff4db35f732d8fb0b00bcad0dc7df93ac5db19
saulr <estifjarris@gmail.com>
2018-10-01 17:22:39 +0200

    MILESTONE-2 Practicing map reduce lambda and rules + tests implemented

--- 08-Milestone_Project_-_2/blackjack/elements/deck.py
+++ 08-Milestone_Project_-_2/blackjack/elements/deck.py
@@ -1,32 +1,5 @@
 import random
-
-
-class PokerCard:
-    ALLOWED_SUITS = ['diamonds', 'spades', 'hearts', 'clubs']
-    ALLOWED_VALUES = range(1, 14)
-
-    def __init__(self, value, suit):
-        self.value = value
-        self.suit = suit
-
-    def __repr__(self):
-        poker_value_dict = {
-            1:  'Ace',
-            2:  'Two',
-            3:  'Three',
-            4:  'Four',
-            5:  'Five',
-            6:  'Six',
-            7:  'Seven',
-            8:  'Eight',
-            9:  'Nine',
-            10: 'Ten',
-            11: 'Jack',
-            12: 'Queen',
-            13: 'King',
-        }
-
-        return '{} of {}'.format(poker_value_dict[self.value], self.suit)
+from blackjack.elements.card import PokerCard
 
 
 class PokerDeck(object):
